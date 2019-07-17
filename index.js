a669b798f68b3a5a72db57d53b03f043783c097a
Apple <apple@Apples-MacBook-Pro-85.local>
2019-07-17 15:48:00 -0700

    weather- Widget

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+var weather = [
+  {
+    condition: 'sunny',
+    lowTemp: '70',
+    highTemp: '76',
+    date: 'Wednesday July 17th',
+    day: 'Monday'
 
+  },
+  {
+    condition: 'cloudy',
+    lowTemp: '67',
+    highTemp: '76',
+    date: 'Thursday July 18th',
+    day: 'Tuesday'
+  }
+]
+
+var dayOne = {
+  condition: 'sunny',
+  lowTemp: '70',
+  highTemp: '76',
+  date: 'Wednesday July 17th',
+  day: 'Monday'
+}
+
+var dayTwo = {
+  condition: 'cloudy',
+  lowTemp: '67',
+  highTemp: '76',
+  date: 'Thursday July 18th',
+  day: 'Tuesday'
+}
+
+for (var i = 0; i < weather.length; i++) {
+  var condition = weather[i].condition
+  var lowTemp = weather[i].lowTemp
+  $('.paragraph').append() // appened text to page
+  $('.paragraph').text() // change text of paragraph
+}
