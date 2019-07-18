9dab0338999a8636ccab575100d18d1540416b26
Apple <apple@Apples-MacBook-Pro-85.local>
2019-07-18 14:00:52 -0700

    first draft

--- index.js
+++ index.js
@@ -1,71 +1,53 @@
-var weather = [
-  {
-    condition: 'sunny',
-    lowTemp: '70',
-    highTemp: '76',
-    date: 'Wednesday July 17th',
-    day: 'Monday'
-
-  },
-  {
-    condition: 'cloudy',
-    lowTemp: '67',
-    highTemp: '76',
-    date: 'Thursday July 18th',
-    day: 'Tuesday'
-  }
-]
-
-var dayOne = {
-  condition: 'sunny',
-  lowTemp: '70',
-  highTemp: '76',
-  date: 'Wednesday July 17th',
-  day: 'Monday'
-}
-
-var dayTwo = {
-  condition: 'cloudy',
-  lowTemp: '67',
-  highTemp: '76',
-  date: 'Thursday July 18th',
-  day: 'Tuesday'
-}
-
-for (var i = 0; i < weather.length; i++) {
-  var condition = weather[i].condition
-  var lowTemp = weather[i].lowTemp
-  $('.paragraph').append() // appened text to page
-  $('.paragraph').text() // change text of paragraph
-}
-
-console.log("click");
-var cardContainer = $('.card-container');
+var tempContainer = $('.temp-container');
+var clothesContainer2 = $('.clothes-container2');
 var submit = $('.submit-btn');
 
-submit.on("click", createNewUser);
+submit.on("click", grabItems);
 
-$('.card-container').on("click", ".delete-btn", deleteItem);
+$('.clothes-container2').on("click", ".delete-btn", deleteItem);
 
-function appendItem(newItem) {
-    cardContainer.append(`
-    <div class="to-do-card">
+function grabItems() {
+  var clothing = $('.newItem').val();
+  var temp=$('.newTemp').val();
+  appendItems(clothing, temp);
+  clearInput();
+}
+
+function appendItems(newItem, newTemp) {
+    clothesContainer2.append(`
+    <div class="clothes-card">
       <p>${newItem}</p> <button class="delete-btn">delete</button>
     </div>
   `);
 }
 
-function createNewUser() {
-  var groceryItem = $('.newItem').val();
-  appendItem(groceryItem);
-  clearInputs();
-}
-
-
-function clearInputs() {
+function clearClothesInput() {
   $('.newItem').val("");
 }
 
 function deleteItem(event){
   event.target.parentNode.remove();
+}
+
+
+tempSubmit.on("click", grabTemp);
+
+$('.temp-container').on("click", ".delete-btn", deleteItem);
+
+function grabTemp() {
+  var temp = $('.newTemp').val();
+  appendTemp(temp);
+  clearTempInput();
+}
+
+function appendTemp(newItem) {
+    tempContainer.append(`
+    <div class="temp-card">
+      <p>${newItem}</p> <button class="delete-btn">delete</button>
+    </div>
+  `);
+}
+
+function clearTempInput() {
+  $('.newItem').val("");
 }
