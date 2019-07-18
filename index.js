c4e60b599334b4e73a1005c85cfc0d7c6c186804
Tiffany Lam <tiffunny@baby-lawnmower.local>
2019-07-18 13:16:13 -0700

    got the button function to work and append the divs with all the input buttons

--- index.js
+++ index.js
@@ -39,16 +39,17 @@
 //   $('.paragraph').text()
 // }
 
-var newDay = ${'.newDayButton'};
-var container = ${'.inputContainer'};
+var newDay = $('.newDayButton');
+var container = $('.inputContainer');
 
 newDay.on("click", addInputBox);
 
 function addInputBox(){ //function to make input boxes appear (still working on it)
   container.append(`
     <div class ="log-card">
-    <input type="paragraph" value="How did it feel outside today?" class="feeling">
-    <input type="paragraph" value="What'd you wear?" class="clothing">
+    <input type ="text"  placeholder="What day is it?" class="day">
+    <input type="paragraph" placeholder="How did it feel outside today?" class="feeling">
+    <input type="paragraph" placeholder="What'd you wear?" class="clothing">
     </div>
 `);
   }
