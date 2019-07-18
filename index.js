479d41b02e5dd757478c6ccd7acd99a1a6e25782
Apple <apple@Apples-MacBook-Pro-85.local>
2019-07-18 14:10:15 -0700

    merge conflict 3

--- index.js
+++ index.js
@@ -1,43 +1,4 @@
-// var weather = [
-//   {
-//     condition: 'sunny',
-//     lowTemp: '70',
-//     highTemp: '76',
-//     date: 'Wednesday July 17th',
-//     day: 'Monday'
-//
-//   },
-//   {
-//     condition: 'cloudy',
-//     lowTemp: '67',
-//     highTemp: '76',
-//     date: 'Thursday July 18th',
-//     day: 'Tuesday'
-//   }
-// ]
-//
-// var dayOne = {
-//   condition: 'sunny',
-//   lowTemp: '70',
-//   highTemp: '76',
-//   date: 'Wednesday July 17th',
-//   day: 'Monday'
-// }
-//
-// var dayTwo = {
-//   condition: 'cloudy',
-//   lowTemp: '67',
-//   highTemp: '76',
-//   date: 'Thursday July 18th',
-//   day: 'Tuesday'
-// }
-//
-// for (var i = 0; i < weather.length; i++) {
-//   var condition = weather[i].condition
-//   var lowTemp = weather[i].lowTemp
-//   $('.paragraph').append()
-//   $('.paragraph').text()
-// }
+
 
 var newDay = $('.newDayButton');
 var container = $('.inputContainer');
@@ -53,29 +14,3 @@ function addInputBox(){ //function to make input boxes appear (still working on 
     </div>
 `);
   }
-
-
-// $('.card-container').on("click", ".delete-btn", deleteItem);
-//
-// function appendItem(newItem) {
-//     cardContainer.append(`
-//     <div class="to-do-card">
-//       <p>${newItem}</p> <button class="delete-btn">delete</button>
-//     </div>
-//   `);
-// }
-//
-// function createNewUser() {
-//   var groceryItem = $('.newItem').val();
-//   appendItem(groceryItem);
-//   clearInputs();
-// }
-//
-//
-// function clearInputs() {
-//   $('.newItem').val("");
-// }
-//
-// function deleteItem(event){
-//   event.target.parentNode.remove();
-// }
