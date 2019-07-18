ce83359d6b6f00192938fd59ee386d80aa7729fd
Tiffany Lam <tiffunny@baby-lawnmower.local>
2019-07-18 10:27:14 -0700

    css and js changes to make new day button and inputs appear, work in progress

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
-var weather = [
-  {
-    condition: 'sunny',
-    lowTemp: '70',
-    highTemp: '76',
-    date: 'Wednesday July 17th',
-    day: 'Monday'
+// var weather = [
+//   {
+//     condition: 'sunny',
+//     lowTemp: '70',
+//     highTemp: '76',
+//     date: 'Wednesday July 17th',
+//     day: 'Monday'
+//
+//   },
+//   {
+//     condition: 'cloudy',
+//     lowTemp: '67',
+//     highTemp: '76',
+//     date: 'Thursday July 18th',
+//     day: 'Tuesday'
+//   }
+// ]
+//
+// var dayOne = {
+//   condition: 'sunny',
+//   lowTemp: '70',
+//   highTemp: '76',
+//   date: 'Wednesday July 17th',
+//   day: 'Monday'
+// }
+//
+// var dayTwo = {
+//   condition: 'cloudy',
+//   lowTemp: '67',
+//   highTemp: '76',
+//   date: 'Thursday July 18th',
+//   day: 'Tuesday'
+// }
+//
+// for (var i = 0; i < weather.length; i++) {
+//   var condition = weather[i].condition
+//   var lowTemp = weather[i].lowTemp
+//   $('.paragraph').append()
+//   $('.paragraph').text()
+// }
 
-  },
-  {
-    condition: 'cloudy',
-    lowTemp: '67',
-    highTemp: '76',
-    date: 'Thursday July 18th',
-    day: 'Tuesday'
+var newDay = ${`.newDayButton`};
+var container = ${`.inputContainer`};
+
+newDay.on("click", addInputBox);
+
+function addInputBox(){ //function to make input boxes appear (still working on it)
+  container.append(
+    <input type="paragraph" value="How did it feel outside today?" class="feeling">
+    <input type="paragraph" value="What'd you wear?" class="clothing">
+
+  )
+
   }
-]
 
-var dayOne = {
-  condition: 'sunny',
-  lowTemp: '70',
-  highTemp: '76',
-  date: 'Wednesday July 17th',
-  day: 'Monday'
-}
-
-var dayTwo = {
-  condition: 'cloudy',
-  lowTemp: '67',
-  highTemp: '76',
-  date: 'Thursday July 18th',
-  day: 'Tuesday'
-}
-
-for (var i = 0; i < weather.length; i++) {
-  var condition = weather[i].condition
-  var lowTemp = weather[i].lowTemp
-  $('.paragraph').append() // appened text to page
-  $('.paragraph').text() // change text of paragraph
 }
